c39d69e06c6b5e9a7fe210c15098d3d8075aed1d
Kayllane  Farias <75839810+Aninimo@users.noreply.github.com>
2023-05-10 14:01:37 -0300

    Create extensions.dart

---
+++ lib/core/extensions.dart
@@ -0,0 +1,8 @@
+import 'package:ecommerce/core/app_data.dart';
+import 'dart:math' show Random;
+
+extension IterableExtension<T> on Iterable<T> {
+  Iterable<E> mapWithIndex<E>(E Function(int index, T value) f) {
+    return Iterable.generate(length).map((i) => f(i, elementAt(i)));
+  }
+}
